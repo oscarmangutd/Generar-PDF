4afd30ee77ea39d06b3cafde90e08e03dae0921a
Oscar Mangut <80204183+oscarmangutd@users.noreply.github.com>
2022-04-12 18:28:46 +0200

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    // Escuchamos el click del botón
+    const $boton = document.querySelector("#btnCrearPdf");
+    $boton.addEventListener("click", () => {
+        const $elementoParaConvertir = document.querySelector("#pdf"); // <-- Aquí puedes elegir cualquier elemento del DOM
+        html2pdf()
+            .set({
+                margin: 1.5,
+                filename: 'documento.pdf',
+                
+                html2canvas: {
+                    scale: 6, // A mayor escala, mejores gráficos, pero más peso
+                    letterRendering: true,
+                },
+                jsPDF: {
+                    unit: "in",
+                    format: "a4",
+                    orientation: 'portrait' // landscape o portrait
+                }
+            })
+            .from($elementoParaConvertir)
+            .save()
+            .catch(err => console.log(err));
+    });
+});
